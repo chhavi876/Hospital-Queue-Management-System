34a0c0533d33e4b0810fceff43a791122c1348a5
chhavi876 <chhavisharma251@gmail.com>
2025-09-16 10:23:29 +0530

    Update routing.py

--- queue_app/routing.py
+++ queue_app/routing.py
@@ -1,10 +1,8 @@
-from django.urls import path
+from django.urls import re_path
 from . import consumers
 
 websocket_urlpatterns = [
-    # Add your WebSocket routes here
-    path(r'ws/queue/updates/$', consumers.QueueConsumer.as_asgi()),
     
-    # Example additional WebSocket route:
-    # re_path(r'ws/counter/(?P<counter_id>\w+)/$', consumers.CounterConsumer.as_asgi()),
+    re_path(r'ws/queue/updates/$', consumers.QueueUpdatesConsumer.as_asgi()),
+    
 ]
